2f2fdcb200cf9e44f92db89cce30adb4acc60e7e
aureuneun <aureuneun@gmail.com>
2021-01-04 11:25:31 +0900

    feat: e2e test

--- src/movies/dtos/create-movie.dto.ts
+++ src/movies/dtos/create-movie.dto.ts
@@ -1,5 +1,5 @@
 import { PickType } from '@nestjs/mapped-types';
-import { Movie } from 'src/movies/entities/movie.entity';
+import { Movie } from '../entities/movie.entity';
 
 export class CreateMovieInput extends PickType(Movie, [
   'title',
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CreateMovieInput } from 'src/movies/dtos/create-movie.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const createMovieInput: CreateMovieInput = {
+    title: 'title',
+    year: 2021,
+    genres: ['genres'],
+  };
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/movies', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send(createMovieInput)
+        .expect(201);
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ ...createMovieInput, other: 'thing' })
+        .expect(400);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/100').expect(404);
+    });
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'updated title' })
+        .expect(200);
+    });
+
+    it('PATCH 404', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/100')
+        .send({ title: 'updated title' })
+        .expect(404);
+    });
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
+
+    it('DELETE 404', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(404);
+    });
   });
 });
